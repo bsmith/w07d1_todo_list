a1602630456f4dbe208440744d9f786709697c5f
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-28 16:16:36 +0000

    Working end-of-task version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState(
+        [
+            { name: "Buy shopping", priority: "high" },
+            { name: "Clean bathroom", priority: "low" },
+            { name: "Car's MOT", priority: "high" }
+        ]
+    );
+
+    const [newTask, setNewTask] = useState("");
+
+    const [newPriority, setNewPriority] = useState("low");
+
+    const changePriority = (index) => {
+        const newTaskArray = [...tasks];
+        const oldPriority = newTaskArray[index].priority;
+        if (oldPriority === "low") {
+            newTaskArray[index].priority = "high";
+        }
+        else if (oldPriority === "high") {
+            newTaskArray[index].priority = "low";
+        }
+        setTasks(newTaskArray)
+
+    }
+
+    const taskNodes = tasks.map((task, index) => {
+        return (
+            <li key={index} className={task.priority === "high" ? "high-priority" : "low-priority"}>
+                {task.name}
+                <div>
+                    <input type="range" id={`priority-${index}`} name={`priority-${index}`}
+                        min="0" max="100" value={task.priority === "high" ? 100 : 0} step="100"
+                        onClick={() => { changePriority(index) }}>
+                    </input>
+                    <label for={`priority-${index}`}>{ task.priority[0].toUpperCase() + task.priority.slice(1) }</label>
+                </div>
+
+
+            </li>
+        )
+    });
+
+    const handleTextChange = (event) => {
+        setNewTask(event.target.value);
+    };
+
+    const handlePriorityChange = (event) => {
+        setNewPriority(event.target.value);
+    }
+
+    const saveNewTask = (event) => {
+        event.preventDefault();
+        const newTasksArray = [...tasks];
+        newTasksArray.push({ name: newTask, priority: newPriority });
+        setTasks(newTasksArray);
+        setNewTask("");
+    };
+
+    return (
+        <>
+            <h1>Todo list</h1>
+
+            <form onSubmit={saveNewTask}>
+                <label htmlFor="new-task">Add new task</label>
+                <textarea id="new-task" value={newTask} onChange={handleTextChange} lines="3"></textarea>
+
+                <input type="radio" id="high" name="priority" value="high" checked={newPriority === "high" ? "checked" : ""} onChange={handlePriorityChange}></input>
+                <label for="high">High</label>
+                <input type="radio" id="low" name="priority" value="low" checked={newPriority === "low" ? "checked" : ""} onChange={handlePriorityChange}></input>
+                <label for="low">Low</label>
+
+                <input type="submit" value="Save new task"></input>
+            </form>
+
+            <ul>
+                {taskNodes}
+            </ul>
+        </>
+    );
 }
 
 export default App;
